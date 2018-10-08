f1a966c59fc7e264381fbcb3dcbc3ecca70ea12e
TheJaox <jirjir30@gmail.com>
2018-10-08 17:50:56 -0500

    jaKSJ

--- FTP/script/filter.py
+++ FTP/script/filter.py
@@ -19,18 +19,18 @@ for line in f:
 		# Create Directories or Files
 		if(sys.argv[3] == str(1)):
 			if(op == 'MKD' or op == 'STOR'):
-				data.append([ip_user, user, date_time, op, file, code])
+				data.append([date_time, ip_user, user, file, op, code])
 		# Download files
 		elif(sys.argv[3] == str(2)):
 			if(op == 'RETR'):
-				data.append([ip_user, user, date_time, op, file, code])
+				data.append([date_time, ip_user, user, file, op, code])
 		# Delete Directories or Files
 		elif(sys.argv[3] == str(3)):
 			if(op == 'DELE' or op == 'RMD'):
-				data.append([ip_user, user, date_time, op, file, code])
+				data.append([date_time, ip_user, user, file, op, code])
 		# Rename Directories or Files
 		elif(sys.argv[3] == str(4)):
 			if(op == 'RNFR' or op == 'RNTO'):
-				data.append([ip_user, user, date_time, op, file, code])
+				data.append([date_time, ip_user, user, file, op, code])
 
-print tabulate(data, headers=['IP', 'User', 'Access', 'Operation', 'File', 'Response Code'])
+print tabulate(data, headers=['Date - Time', 'IP User', 'User Name', 'File', 'Operation', 'Response Code'])
